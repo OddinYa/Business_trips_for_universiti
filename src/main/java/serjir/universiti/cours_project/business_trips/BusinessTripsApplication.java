c78a201fd1d795fd9f849ad7e586a55835df6673
OddinYa <oddinserj@gmail.com>
2023-09-11 21:58:27 +0300

    first commit

---
+++ src/main/java/serjir/universiti/cours_project/business_trips/BusinessTripsApplication.java
@@ -0,0 +1,13 @@
+package serjir.universiti.cours_project.business_trips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessTripsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessTripsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/serjir/universiti/cours_project/business_trips/BusinessTripsApplicationTests.java
@@ -0,0 +1,13 @@
+package serjir.universiti.cours_project.business_trips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessTripsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

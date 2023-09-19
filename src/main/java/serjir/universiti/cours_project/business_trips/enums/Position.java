182110cbdbb5b9e44c85611f81a8b7c00c4b5c87
OddinYa <oddinserj@gmail.com>
2023-09-20 00:24:02 +0300

    task is ready

--- src/main/java/serjir/universiti/cours_project/business_trips/enums/Position.java
+++
@@ -1,7 +0,0 @@
-package serjir.universiti.cours_project.business_trips.enums;
-
-public enum Position {
-    CHIEF,
-    MANAGER,
-    WORKER
-}
--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,2 +1,3 @@
-INSERT INTO trip (start_date, end_date) VALUES ('2023-09-16', '2023-09-20');
-INSERT INTO trip (start_date, end_date) VALUES ('2023-08-20', '2023-09-24');
+INSERT INTO trip (start_date, end_date,city) VALUES ('2023-09-16', '2023-09-20','Пенза');
+INSERT INTO trip (start_date, end_date,city) VALUES ('2023-08-20', '2023-09-24','Казань');
+
